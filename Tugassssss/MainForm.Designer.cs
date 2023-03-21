8554c0a7368d777255f76aa0f36985270ae52062
Yogi <126434742+ciarunika@users.noreply.github.com>
2023-03-21 22:34:57 +0700

    Add files via upload

---
+++ Tugassssss/MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Yenyin
+ * Date: 21/03/2023
+ * Time: 20:51
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Tugassssss
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.panel1.Location = new System.Drawing.Point(24, 23);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(630, 350);
+			this.panel1.TabIndex = 0;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1Paint);
+			this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Panel1MouseClick);
+			this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1MouseDown);
+			this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel1MouseMove);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(716, 40);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(193, 44);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "LINE";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(834, 340);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(110, 33);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "EXIT";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(686, 340);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(110, 33);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Clear";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(686, 124);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "X";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(686, 166);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Y";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(686, 219);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "LENGHT";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(809, 124);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 7;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(809, 163);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 8;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(809, 216);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 9;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(961, 395);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Name = "MainForm";
+			this.Text = "Tugassssss";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Tugassssss/MainForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tugassssss{
+
+	public partial class MainForm : Form{
+		
+		private Graphics objgraphic;
+		private bool gambar = false, dragmode = false;
+		private int X1, Y1, x, y, X2, Y2;
+		private double xy;
+		private int loop=0, line=0 ;
+		
+		public MainForm(){
+
+			InitializeComponent();
+		}
+		void MainFormLoad(object sender, EventArgs e){
+			
+			objgraphic = panel1.CreateGraphics();
+		}
+		
+		void Button1Click(object sender, EventArgs e){
+			
+			line = 1;
+		}
+		void Button2Click(object sender, EventArgs e){
+			
+			Environment.Exit(0);
+		}
+		void Button3Click(object sender, EventArgs e){
+			
+			this.Refresh();
+			textBox1.Text = "";
+			textBox2.Text = "";
+			textBox3.Text = "";
+		}
+		
+		void Panel1Paint(object sender, PaintEventArgs e){
+			
+			objgraphic = panel1.CreateGraphics();
+		}
+		
+		void Panel1MouseClick(object sender, MouseEventArgs e){
+			
+			if (gambar == true && dragmode == true){
+
+				x = e.X;
+				y = e.Y;
+				X2 = e.X - X1;
+				Y2 = Y1 - e.Y;
+
+				if (line == 1){
+	
+					objgraphic.DrawLine(new Pen(Color.Black), X1, Y1, e.X, e.Y);
+				}
+
+				else {
+
+					MessageBox.Show("KLICK TOMBOL LINE");
+				}
+			}
+		}
+		
+		void Panel1MouseDown(object sender, MouseEventArgs e){
+			
+			if (e.Button == MouseButtons.Left){
+
+				gambar = true;
+				loop++;
+			}
+			
+			X1 = e.X;
+			Y1 = e.Y;
+			dragmode = true;
+		}
+		
+		void Panel1MouseMove(object sender, MouseEventArgs e){
+			
+			textBox1.Text = Convert.ToString(X2);
+			textBox2.Text = Convert.ToString(Y2);
+			xy = Math.Sqrt((X2 * X2) + (Y2 * Y2));
+			textBox3.Text = Convert.ToString(xy);
+		}
+		
+		void TextBox1TextChanged(object sender, EventArgs e){
+		}		
+	}
+}
